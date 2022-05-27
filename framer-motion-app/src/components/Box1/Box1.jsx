3a26479d8fba520c5416e4a318652b8dd890d1cc
Angel Cochachez <angelcochachez@Angels-MacBook-Pro.local>
2022-05-27 15:25:26 -0700

    Imported frame motion into components via - framer motion

--- framer-motion-app/src/components/Box1/Box1.jsx
+++ framer-motion-app/src/components/Box1/Box1.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { motion } from "framer-motion"
 
 export default function Box1() {
   return (
     <div className="box-container">
-      <div className="box"></div>
+      <motion.div 
+        className="box"
+        animate={{
+            x:500,
+            opacity:1
+      }}
+      initial={{
+          opacity:0.1
+      }}
+      transition={{
+          type:"spring",
+          stiffness:60
+
+      }}
+      >
+      
+      </motion.div>
     </div>
   );
 }
