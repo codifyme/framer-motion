b0fff1d9a064cb24b667d17b30fc1064fdb7124a
Angel Cochachez <angelcochachez@Angels-MacBook-Pro.local>
2022-06-01 02:34:49 -0700

    Added list and box variants for motion animation

--- framer-motion-app/src/components/Box3/Box3.jsx
+++ framer-motion-app/src/components/Box3/Box3.jsx
@@ -3,30 +3,51 @@ import { motion } from "framer-motion"
 
 
 export default function Box3() {
-
     const boxVariant={
-        lion:{
-            x:100,
-            scale:1.5,
-            backgroundColor:"yellow"
+        hidden:{
+            x:"-100vw"
         },
-        elephant:{
-            X:1000,
-            scale:0.3,
-            backgroundColor:"green"
+        visible:{
+            x:0,
+            transition:{
+                delay:0.5,
+                when:"beforeChildren",
+                staggerChildren:0.2
+            }
         }
-    
     }
 
-    return (
-    <div className="box-container">
-      <motion.div 
-        className="box"
-        variants={boxVariant}
-        initial="lion"
-        animate="elephant">
+    const listVariant={
+        hidden:{
+            x:-10,
+            opacity:0
+        },
+        visible:{
+            x:0,
+            opacity:1,
+            staggerChildren:0.2
+             
+        }
+    }
+    
 
-      </motion.div>
-    </div>
-  );
+    return (
+        <div className="box-container">
+            <motion.div 
+                className="box"
+                variants={boxVariant}
+                animate="visible"
+                initial="hidden"
+            >
+                {[1,2,3].map(box=>{
+                    return (
+                    <motion.li 
+                        className="boxItem"
+                        variants={listVariant}
+                     ></motion.li>
+                     )
+                })}
+            </motion.div>
+        </div>
+    );
 }
