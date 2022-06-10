ac3b2ea1f09e320950d0305fb7f36db19d1eefa4
Angel Cochachez <angelcochachez@Angels-MacBook-Pro.local>
2022-06-10 13:30:39 -0700

    Used Animate Hook

--- framer-motion-app/src/components/Box5/Box5.jsx
+++ framer-motion-app/src/components/Box5/Box5.jsx
@@ -11,13 +11,30 @@ export default function Box5() {
             control.start({
                 x:1500,
                 transition:{duration:2}
-            })
+            }) 
         }}>Move Right</button>
         
-        <button>Move Left</button>
-        <button>Circle</button>
-        <button>Square</button>
-        <button>Stop</button>
+        <button onClick={()=>{
+            control.start({
+                x:0,
+                transition:{duration:2}
+            })
+        }}>Move Left</button>
+        <button onClick={()=>{
+            control.start({
+                borderRadius:"50%",
+                transition:{duration:2}
+            })
+        }}>Circle</button>
+        <button onClick={()=>{
+            control.start({
+                borderRadius:0,
+                transition:{duration:1}
+            })
+        }}>Square</button>
+        <button onClick={()=>{
+            control.stop()
+        } }>Stop</button>
         
       <motion.div 
         className="box"
